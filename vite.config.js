f4d64e628086f775070d5e60ca8665ab3d6adbcc
OlesiaKubska <kublesia0908@gmail.com>
2024-05-28 14:30:14 +0200

    Configured Vite for GitHub Pages deployment and added deployment script

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5000, // Change this to your desired port
+ plugins: [react()],
+ server: {
+  port: 5000, // Change this to your desired port
+ },
+ resolve: {
+  alias: {
+   src: "/src",
+   components: "/src/components",
+   pages: "/src/pages",
   },
+ },
+ base: "/online-shopper/",
 });
